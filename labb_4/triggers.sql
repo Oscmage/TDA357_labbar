a97c322b3d1fa8a3a0be4d09a5cae9bb4acc3902
Oscar Evertsson <oscar.evertsson@live.com>
2016-03-08 18:50:07 +0100

    Removed use of StudentsFirstInQueue and remove the whole view since it might confuse the retard.

--- labb_4/triggers.sql
+++ labb_4/triggers.sql
@@ -65,14 +65,6 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER register INSTEAD OF INSERT ON Registrations
     FOR EACH ROW EXECUTE PROCEDURE register();
 
-
---Trigger two
-CREATE OR REPLACE VIEW StudentsFirstInQueue AS
-	SELECT cqp.code,cqp.personal_number,name 
-	FROM CourseQueuePositions AS cqp
-	JOIN students ON students.personal_number = cqp.personal_number
-	WHERE cqp.position = '1'; 
-
 CREATE OR REPLACE FUNCTION unregister() RETURNS trigger AS $$
 	DECLARE
 		isLimitedCourse int;
@@ -80,7 +72,6 @@ CREATE OR REPLACE FUNCTION unregister() RETURNS trigger AS $$
 		currentReg int;
 		totWaitingStudents int;
 		firstPersNum TEXT;	
-		firstPersName TEXT;
 	BEGIN
 		-- Delete student from both possible table
 		DELETE FROM waiting_for WHERE code = OLD.code AND personal_number = OLD.personal_number;
@@ -103,7 +94,7 @@ CREATE OR REPLACE FUNCTION unregister() RETURNS trigger AS $$
 					-- we now know there's someone to register.
 
 					-- Get the student which was first in queue.
-					firstPersNum := (SELECT personal_number FROM StudentsFirstInQueue AS SFIQ WHERE OLD.code = SFIQ.code);
+					firstPersNum := (SELECT personal_number FROM Registrations AS REG WHERE OLD.code = REG.code AND position = '1');
 
 					-- Delete the student which was first in queue from the waiting list
 					DELETE FROM waiting_for WHERE code = OLD.code AND personal_number = firstPersNum;
